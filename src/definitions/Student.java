6352fcf557e7d16171003ab6adebb6e3d7d5d3e4
Himanshu Gaur <himanshu.gaur_cs19@gla.ac.in>
2020-08-28 19:39:41 +0530

    add getters and setters methods to Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -15,4 +15,67 @@ public class Student {
 	private long int universityRollNumber;
 	private int bookIssued;
 	private String[] book;
+	
+	
+
+
+	public void setFirstName(String firstName) {
+	
+		this.firstName = firstName;
 	}
+
+	public String getFirstName() {
+	
+		return firstName;
+	}
+
+	public void setMiddleName(String middleName) {
+	
+		this.middleName = middleName;
+	}
+
+	public String getMiddleName() {
+	
+		return middleName;
+	}
+
+	public void setLastName(String lastName) {
+	
+		this.lastName = lastName;
+	}
+
+	public String getLastName() {
+	
+		return lastName;
+	}
+
+	public void setUniversityRollNumber(int universityRollNumber) {
+	
+		this.universityRollNumber = universityRollNumber;
+	}
+
+	public int getUniversityRollNumber() {
+	
+		return universityRollNumber;
+	}
+
+	public void setBookIssued(int bookIssued) {
+	
+		this.bookIssued = bookIssued;
+	}
+
+	public int getBookIssued() {
+	
+		return bookIssued;
+	}
+
+	public void setBook(String[] book) {
+	
+		this.book = book;
+	}
+
+	public String[] getBook() {
+	
+		return book;
+    }
+}
