10e9299fb5fce30bcb290047a810a4b245099021
Himanshu Gaur <himanshu.gaur_cs19@gla.ac.in>
2020-08-28 19:16:28 +0530

    add required fields to Student.java

--- src/definitions/Student.java
+++ src/definitions/Student.java
@@ -1,10 +1,18 @@
-/*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
+/*  User: Himanshu Gaur
  *  Date: 21/08/20
  *  Time: 3:49 PM
  *  File Name : Student.java
  * */
 package definitions;
 
+import java.util.Arrays;
+
 public class Student {
-}
+	
+	private String firstName;
+	private String middleName;
+	private String lastName;
+	private long int universityRollNumber;
+	private int bookIssued;
+	private String[] book;
+	}
