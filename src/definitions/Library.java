82d8086d5d81521bcb141ad662ee0769655644ff
Himanshu Gaur <himanshu.gaur_cs19@gla.ac.in>
2020-08-29 20:56:13 +0530

    add constructor() methods to Library class

--- src/definitions/Library.java
+++ src/definitions/Library.java
@@ -8,4 +8,21 @@ package definitions;
 public class Library {
 
 	private Book[] availableBooks;
+	
+	// constructor() method without parameter.
+	
+	public Library() {
+        this.availableBook = new Book[10000];
+        for (int i = 0; i < availableBook.length; i++) {
+            availableBook[i] = new Book("Book " + (i + 1));
+        }
+    }
+
+	// constructor() method with parameter.
+	
+	public Library(Book[] availableBooks)
+	{
+		this.availableBooks = availableBooks;
+	}
+	
 }
