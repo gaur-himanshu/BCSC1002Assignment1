a62d60a54a59e7b6a1ec36a05d2639f262101a5d
Himanshu Gaur <himanshu.gaur_cs19@gla.ac.in>
2020-08-30 17:46:29 +0530

    initialize all the constant value as requirement in FrontDesk class

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -10,6 +10,12 @@ import definitions.Student;
 import java.util.Scanner;
 
 public class FrontDesk {
+	
+	private static final int ISSUE_A_NEW_BOOK = 1;
+	private static final int RETURN_PREVIOUS_BOOK = 2;
+	private static final int SHOW_ALL_ISSUED_BOOKS = 3;
+	private static final int EXIT = 4;
+	
     public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 	    int studentInput;
@@ -21,7 +27,7 @@ public class FrontDesk {
 		System.out.println("2. Return a previously issues book for me.");
 		System.out.println("3. Show me all my issues books.");
 		System.out.println("4. Exit.");
-		
+		System.out.println("Please choose from 1 to 4 as per your requirement");
 		}
     }
 }
