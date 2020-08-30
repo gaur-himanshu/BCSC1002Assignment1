7ff392dd4381d6a5732eb7f2e3de9589bbcd0ffb
Himanshu Gaur <himanshu.gaur_cs19@gla.ac.in>
2020-08-30 15:44:45 +0530

    final commit

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -6,8 +6,7 @@
 package definitions;
 
 public class Book {
-
-
+	
 	private String bookName;
 	private String bookAuthor;
 	private String iSBNNumber;
@@ -15,7 +14,6 @@ public class Book {
 	// constructer() method without parameter.
 
 	public Book() {
-	
 		bookName = " ";
 		bookAuthor = " ";
 		iSBNNumber = "0000000000000";
@@ -24,77 +22,55 @@ public class Book {
 	// Costructer() method with parameter.
 
 	public Book(String bookName, String bookAuthor, String iSBNNumber) {
-	
 		this.bookName = bookName;
 		this.bookAuthor = bookAuthor;
 		if (iSBNNumber.length() == 13) {
-		
 			this.iSBNNumber = iSBNNumber;
 		}
 		else {
-		
 			System.out.println("please enter 13 digit number");
 		}
-
 	}
 
-
-
-
 	public void setBookName(String bookName) {
-	
-
 		this.bookName = bookName;
 	}
 
 	public String getBookName() {
-	
-
 		return bookName;
 	}
 
 	public void setBookAuthor(String bookAuthor) {
-	
-
 		this.bookAuthor = bookAuthor;
 	}
 
 	public String getBookAuthor() {
-	
-
 		return bookAuthor;
 	}
 
 	public void setISBNNumber(String iSBNNumber) {
-	
-		
-
 		if (iSBNNumber.length() == 13) {
-		
 			this.iSBNNumber = iSBNNumber;
 		}
 		else {
-		
 			System.out.println("Please enter the correct ISBN number,it should be of 13 digit");
-
 		}
-
-
 	}
 
 	public String getISBNNumber() {
-	
-
 		return iSBNNumber;
 	}
 	// this tostring() method converts the object to string.
+	
 	public String toString() {
         return String.format("Book Name: %s, Book Author: %s, ISBN Number: %s", getBookName(), getBookAuthor(), getISBNNumber());
 	}
+	
 	// the equals() and the hashcode() methods.
+	
     @Override
     public boolean equals(Object o) {
-        if (this == o) { // video == video1
+        if (this == o) { 
             return true;
         }
         if (o == null || this.getClass() != o.getClass()) {
