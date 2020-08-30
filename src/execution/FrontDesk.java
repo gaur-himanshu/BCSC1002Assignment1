d70fd7352cf2a83ee44f74333b9c2e983b943371
Himanshu Gaur <himanshu.gaur_cs19@gla.ac.in>
2020-08-30 17:40:36 +0530

    declaration of required fields and prints the initial comments for end user in FrontDesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -1,13 +1,27 @@
-/*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
+/* User: Himanshu Gaur
+ *  Date: 30/08/20
  *  Time: 3:50 PM
  *  File Name : FrontDesk.java
  * */
 package execution;
 
+import definitions.Student;
+
+import java.util.Scanner;
+
 public class FrontDesk {
     public static void main(String[] args) {
-
+		Scanner scanner = new Scanner(System.in);
+	    int studentInput;
+	    Student student = new Student();
+	    do {
+		System.out.println("-=-=--=-=-\"Welcome To The Front Desk\"-=-=--=-=-");
+		System.out.println("How may I help you today?");
+		System.out.println("1. Issue a new book for me.");
+		System.out.println("2. Return a previously issues book for me.");
+		System.out.println("3. Show me all my issues books.");
+		System.out.println("4. Exit.");
+		
+		}
     }
 }
