43cc82fd17d52595fc0fcdc21eb079cc2bb1fae8
Himanshu Gaur <himanshu.gaur_cs19@gla.ac.in>
2020-08-30 18:36:07 +0530

    final commit

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -12,7 +12,7 @@ import java.util.Scanner;
 public class FrontDesk {
 	
 	private static final int ISSUE_A_NEW_BOOK = 1;
-	private static final int RETURN_PREVIOUS_BOOK = 2;
+	private static final int RETURN_THE_PREVIOUS_BOOK = 2;
 	private static final int SHOW_ALL_ISSUED_BOOKS = 3;
 	private static final int EXIT = 4;
 	
@@ -43,7 +43,7 @@ public class FrontDesk {
 					System.out.println("Please! Enter the book name do you want to issue");
 					student.addBook(scanner.nextLine());
 					break;
-		        case RETURN_PREVIOUS_BOOK:
+		        case RETURN_THE_PREVIOUS_BOOK:
 		            System.out.println("Enter the name of book do you want to return")
 		            sudent.returnBook(scanner.nextLine());
 		            break;
